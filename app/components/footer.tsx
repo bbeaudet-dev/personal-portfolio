aad62b14fd118c778a2e6eca648fc25870f28639
Ben Beaudet <bbeaudet0@gmail.com>
2025-06-08 15:02:34 -0400

    removed example posts

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -24,18 +24,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="/rss"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">rss</p>
-          </a>
-        </li>
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://github.com/vercel/next.js"
+            href="https://github.com/bbeaudet-dev/personal-portfolio"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">github</p>
@@ -46,16 +35,38 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
+            href="https://x.com/ben_beaudet"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">tweets</p>
+          </a>
+        </li>
+        {/* <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="/rss"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">rss</p>
+          </a>
+        </li> */}
+        {/* <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
             href="https://vercel.com/templates/next.js/portfolio-starter-kit"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">view source</p>
           </a>
-        </li>
+        </li> */}
       </ul>
-      <p className="mt-8 text-neutral-600 dark:text-neutral-300">
+      {/* <p className="mt-8 text-neutral-600 dark:text-neutral-300">
         © {new Date().getFullYear()} MIT Licensed
-      </p>
+      </p> */}
     </footer>
   )
 }
