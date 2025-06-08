2c856fa45da2519f11a6a5aabf34c9feeed5774f
Ben Beaudet <bbeaudet0@gmail.com>
2025-06-08 15:06:42 -0400

    updated home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,16 @@ export default function Page() {
         just another blog
       </h1>
       <p className="mb-4">
-        {`Hey, I'm Ben Beaudet. \
-        \
-        I began pivoting from mechanical engineering to software engineering a few months ago and found that helps me get better at explaining my thought process, understanding new concepts, and processing both my insights and my struggles. \
-        \
-        From June 2nd to August 30th 2025, I will be doing the [Fractal Bootcamp & AI Accelerator](https://fractalbootcamp.com/) in Brooklyn, NY. Most of my posts will be about coding and game dev, especially during this 3-month window, but I like to explore all kinds of topics and want this to be an experiment for both what I enjoy writing about and what you might want to read. \
-        
-        Welcome and thanks for being here.`}
+        {`Hey, I'm Ben Beaudet.`}
+      </p>
+      <p className="mb-4">
+        {`I began pivoting from mechanical engineering to software engineering a few months ago and found that helps me get better at explaining my thought process, understanding new concepts, and processing both my insights and my struggles.`}
+      </p>
+      <p className="mb-4">
+        {`From June 2nd to August 30th 2025, I will be doing the [Fractal Bootcamp & AI Accelerator](https://fractalbootcamp.com/) in Brooklyn, NY. Most of my posts will be about coding and game dev, especially during this 3-month window, but I like to explore all kinds of topics and want this to be an experiment for both what I enjoy writing about and what you might want to read.`}
+      </p>
+      <p className="mb-4">
+        {`Welcome and thanks for being here.`}
       </p>
       <div className="my-8">
         <BlogPosts />
