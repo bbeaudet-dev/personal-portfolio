ead41e129d7c74ebc66ede670ac40ee6fe0ce401
Ben Beaudet <bbeaudet0@gmail.com>
2025-06-08 15:17:57 -0400

    nvm the home page link doesn't work

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,7 @@ export default function Page() {
         {`I began pivoting from mechanical engineering to software engineering a few months ago and found that helps me get better at explaining my thought process, understanding new concepts, and processing both my insights and my struggles.`}
       </p>
       <p className="mb-4">
-        {`From June 2nd to August 30th 2025, I will be doing the 
-        <a href="https://fractalbootcamp.com/">
-          Fractal Bootcamp & AI Accelerator 
-        </a>
-        in Brooklyn, NY. Most of my posts will be about coding and game dev, especially during this 3-month window, but I like to explore all kinds of topics and want this to be an experiment for both what I enjoy writing about and what you might want to read.`}
+        {`From June 2nd to August 30th 2025, I will be doing the Fractal Bootcamp & AI Accelerator in Brooklyn, NY. Most of my posts will be about coding and game dev, especially during this 3-month window, but I like to explore all kinds of topics and want this to be an experiment for both what I enjoy writing about and what you might want to read.`}
       </p>
       <p className="mb-4">
         {`Welcome and thanks for being here.`}
