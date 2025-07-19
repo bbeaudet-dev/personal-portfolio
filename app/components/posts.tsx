1ecd7bb4a9c1559a2f4941f94bf78a98d4aeb0eb
Ben Beaudet <bbeaudet0@gmail.com>
2025-07-19 17:26:18 -0400

    week 3/4 posts

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -21,8 +21,8 @@ export function BlogPosts() {
             className="flex flex-col space-y-1 mb-4 blog-post-link"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
+            <div className="w-full grid grid-cols-1 md:grid-cols-[120px_1fr] gap-1 md:gap-6">
+              <p className="text-neutral-600 dark:text-neutral-400 tabular-nums whitespace-nowrap">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
               <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
